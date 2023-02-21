6b2dec1511865f3f46a77bb49627d96711a1690a
lucas <lcs_demais@hotmail.com>
2023-02-21 09:48:28 -0300

    att

--- aula_03/exercicio_02.py
+++ aula_03/exercicio_02.py
@@ -1,5 +1,7 @@
-a = 1
-b = 2
+numero1 = int(input("Digite um valor inteiro: "))
+testa_par = numero1 % 2 == 0
+if(testa_par):
+    print("O número é par")
+else:
+    print("O número é ìmpar")
 
-if(a == b):
-    print('É igual')
