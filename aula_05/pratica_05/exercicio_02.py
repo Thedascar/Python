48aae1375982a51b2e4030cff07282f61251cb49
lucas <lcs_demais@hotmail.com>
2023-03-02 22:01:14 -0300

    att

--- aula_05/pratica_05/exercicio_02.py
+++ aula_05/pratica_05/exercicio_02.py
@@ -11,9 +11,53 @@ print('|--------------------|')
 print('|Meus Games Favorites|')
 print('#--------------------#')
 
+def existeArquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+def criarArquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo,'wt+')
+        a.close()
+    except:
+        print('Erro na criação do arquivo')
+    else:
+        print(f'Arquivo {nomeArquivo} foi criado com sucesso')
+
+def cadastrarJogo(nomeArquivo, nomeJogo, nomeVideoGame):
+    try:
+        a = open(nomeArquivo,'at')
+    except:
+        print('Erro ao abrir o arquivo')
+    else:
+        a.write(f'{nomeJogo};{nomeVideoGame}\n')
+    finally:
+        a.close()
+def listarArquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo, 'rt')
+    except:
+        print('Erro ao listar jogos')
+    else:
+        print(a.read())
+    finally:
+        a.close
 
 
 arquivo = 'games.txt'
+if existeArquivo(arquivo):
+    print('Arquivo localizado no computador')
+else:
+    print('Arquivo Inexistente')
+    criarArquivo(arquivo)
+
+
+
+
 
 while True:
     print('1 - Cadastrar novo item')
@@ -23,8 +67,12 @@ while True:
     op = valida_int('Escolha a opção desejada: ',1,3)
     if op == 1:
         print('Cadastrar game')
+        nomeJogo = input('Nome do jogo: ')
+        nomeVideoGame = input('Nome do Video Game: ')
+        cadastrarJogo(arquivo,nomeJogo,nomeVideoGame)
     elif op == 2:
         print('Listar Games')
+        listarArquivo(arquivo)
     elif op == 3:
         print('Sair')
         break
